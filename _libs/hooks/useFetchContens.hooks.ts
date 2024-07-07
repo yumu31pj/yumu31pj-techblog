3a7f575517fa0e1d4bd67003e917095f18853f53
yumu31 <yumu31pj@gmail.com>
2024-07-07 21:28:21 +0900

    feat: microCMS Fetch Test

---
+++ _libs/hooks/useFetchContens.hooks.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { getMicroCMSConnection } from "../utils/getMicroCMSClient";
+const useFetchContens = (
+  endpointId: string,
+) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [response, setResponse] = useState<any>();
+  const [hasError, setHasError] = useState(false);
+  const errorMessage = "Failed to fetch contents";
+
+  useEffect(() => {
+    const client = getMicroCMSConnection();
+
+    const getContentsList = async() => {
+      try {
+        const apiResponse = await client.get({
+          endpoint: endpointId,
+          queries: {
+          }
+        });
+        setResponse(apiResponse)
+      } catch (e) {
+        console.error(e);
+        setHasError(true);
+      } finally {
+        setIsLoading(false)
+      }
+    };
+    getContentsList();
+  },[])
+  return {isLoading, response, hasError, errorMessage, endpointId};
+}
+
+export default useFetchContens;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
+'use client';
+
+import useFetchContens from "@/_libs/hooks/useFetchContens.hooks";
+import parse from "html-react-parser";
+import { Suspense } from "react";
+
 const Home = () => {
+  const endpoint = 'blog';
+
+  const {isLoading, response, hasError, errorMessage} = useFetchContens(endpoint);
+
+  if (isLoading) {
+    return (
+      <p>Now Loading...</p>
+    )
+  }
+  if (hasError) {
+    return (
+      <p>{errorMessage}</p>
+    )
+  }
+
+  console.log(JSON.stringify(response.contents));
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Suspense>
+        {response.contents && response.contents.map((item:any, key:number) => (
+          <article key={key}>
+            <h3>{item.title}</h3>
+            <div>{parse(item.content)}</div>
+            {item.categories ? (
+              item.categories.map((category: string, key: number) => (
+                <div key={key}>{category}</div>
+              ))
+            ) : null}
+          </article>
+        ))}
+      </Suspense>
+    </>
   );
 }
 
