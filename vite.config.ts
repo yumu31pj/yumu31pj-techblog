d34b9d1c801c82793dd1ca91783555023c86f513
yumu31 <yumu31pj@gmail.com>
2025-03-21 13:01:19 +0900

    Update css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
+import dotenv from 'dotenv';
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
