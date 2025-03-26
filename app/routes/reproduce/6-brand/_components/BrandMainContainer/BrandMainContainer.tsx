9fd0b8326ec9599606848234d000311bb78acdf3
yumu31 <yumu31pj@gmail.com>
2025-03-26 17:44:33 +0900

    Update style for 6-brand

--- app/routes/reproduce/6-brand/_components/BrandMainContainer/BrandMainContainer.tsx
+++ app/routes/reproduce/6-brand/_components/BrandMainContainer/BrandMainContainer.tsx
@@ -2,10 +2,8 @@ import styles from "./BrandMainContainer.module.scss";
 
 export const BrandMainContainer = ({children} : {children: React.ReactNode}) => {
   return (
-    <div className={styles["l-main-container"]}>
-    {/* <main className={styles["l-main-container"]}> */}
+    <main className={styles["l-main-container"]}>
       {children}
-    {/* </main> */}
-    </div>
+    </main>
   )
 }
